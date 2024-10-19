4e818ebd2245ffa950a2316bec9241e00fd086ba
ismail <ismailalehyen@gmail.com>
2024-10-19 09:52:19 +0100

    products viewset

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from products.models import Product
+from products.serializers import ProductSerializer
+
+class ProductViewset(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
